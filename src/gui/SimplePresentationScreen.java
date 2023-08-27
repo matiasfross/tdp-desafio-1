8fa04b0d30a9b5749aa2fe995b2037cb4c2d1dfc
matiasross <rossmatias8@gmail.com>
2023-08-27 14:29:41 -0300

    Version final

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -22,6 +22,9 @@ import entities.Student;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import java.awt.Toolkit;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -33,8 +36,15 @@ public class SimplePresentationScreen extends JFrame {
 	private Student studentData;
 	private JPanel panel;
 	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_2;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	public SimplePresentationScreen(Student studentData) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
@@ -57,14 +67,92 @@ public class SimplePresentationScreen extends JFrame {
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
 		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		
+		lblNewLabel_2 = new JLabel("LU");
+		
+		JLabel lblNewLabel_3 = new JLabel("Apellido");
+		
+		JLabel lblNewLabel_4 = new JLabel("Nombre");
+		
+		JLabel lblNewLabel_5 = new JLabel("E-mail");
+		
+		JLabel lblNewLabel_6 = new JLabel("Github URL");
+		
+		textField = new JTextField(Integer.toString(studentData.getId()));
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField(studentData.getLastName());
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField(studentData.getFirstName());
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField(studentData.getMail());
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField(studentData.getGithubURL());
+		textField_4.setColumns(10);
 		GroupLayout gl_tabInformation = new GroupLayout(tabInformation);
 		gl_tabInformation.setHorizontalGroup(
 			gl_tabInformation.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 425, Short.MAX_VALUE)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_4))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_5))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addGap(10)
+							.addComponent(lblNewLabel_6))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_2))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_3)))
+					.addGap(32)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE)
+							.addContainerGap())
+						.addGroup(Alignment.LEADING, gl_tabInformation.createSequentialGroup()
+							.addComponent(textField, 320, 320, 320)
+							.addContainerGap())
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addGroup(gl_tabInformation.createParallelGroup(Alignment.TRAILING)
+								.addComponent(textField_3, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE)
+								.addComponent(textField_2, Alignment.LEADING, 320, 320, Short.MAX_VALUE)
+								.addComponent(textField_4, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE))
+							.addContainerGap())))
 		);
 		gl_tabInformation.setVerticalGroup(
 			gl_tabInformation.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 173, Short.MAX_VALUE)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_2)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_3)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_4)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblNewLabel_5))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addGap(9)
+							.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(9)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_6)
+						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
 		);
 		tabInformation.setLayout(gl_tabInformation);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
