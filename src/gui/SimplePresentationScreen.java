9b03e4d9f115689dab7a8c4bf1be514e661d7c11
matiasross <rossmatias8@gmail.com>
2023-08-27 12:30:53 -0300

    Imagen agregada y registro de fecha

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -2,21 +2,37 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
+import javax.imageio.ImageIO;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+import javax.swing.GroupLayout.Alignment;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel tabInformation;
+	
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JPanel panel;
+	private JLabel lblNewLabel;
 
 	public SimplePresentationScreen(Student studentData) {
 		this.studentData = studentData;
@@ -32,6 +48,7 @@ public class SimplePresentationScreen extends JFrame {
 		setContentPane(contentPane);
 		
 		init();
+		setVisible(true);
 	}
 	
 	private void init() {
@@ -40,6 +57,36 @@ public class SimplePresentationScreen extends JFrame {
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
 		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		GroupLayout gl_tabInformation = new GroupLayout(tabInformation);
+		gl_tabInformation.setHorizontalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 425, Short.MAX_VALUE)
+		);
+		gl_tabInformation.setVerticalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 173, Short.MAX_VALUE)
+		);
+		tabInformation.setLayout(gl_tabInformation);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
+		
+		
+		panel = new JPanel();
+		
+		contentPane.add(panel, BorderLayout.EAST);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		lblNewLabel = new JLabel();
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(new ImageIcon(SimplePresentationScreen.class.getResource("/images/nerdfacecover.jpg")).getImage().getScaledInstance(150, 150,Image.SCALE_DEFAULT)));
+		panel.add(lblNewLabel);
+		
+		
+		crearRegistroFecha();
+
+	}
+	
+	private void crearRegistroFecha() {
+		JLabel lblNewLabel_1 = new JLabel("Esta ventana fue generada el " + new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime()) + " a las: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+		contentPane.add(lblNewLabel_1, BorderLayout.SOUTH);
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,16 @@
 package launcher;
+import javax.swing.JFrame;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	Student student = new Student(140332 , "Ross" , "Matias" , "rossmatias8@gmail.com" , "https://github.com/matiasfross" , "/Desafio1/src/images/nerdfacecover.jpg");
+            	JFrame app = new SimplePresentationScreen(student);
             }
         });
     }
